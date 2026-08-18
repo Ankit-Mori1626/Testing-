baf4de7aae9ce8d5ca746491745fbd0ded659ab4
Ankit Mori <ankitmori2323@gmail.com>
2026-08-18 18:13:44 +0530

    first commit

--- database/init.sql
+++
@@ -1,6 +0,0 @@
--- This runs automatically once, the first time the Postgres container starts,
--- because it's placed in /docker-entrypoint-initdb.d/ (see Dockerfile).
--- Django's migrations will create the actual app tables; this is just a placeholder
--- for anything you want pre-seeded (extensions, roles, etc).
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
